0a35f6d6d25d0e88b56857fd95741e539757d633
Max <maxstill2006@gmail.com>
2024-06-29 17:04:26 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void reverseString(char* start, char* end) {
+    char temp;
+    while (start < end) {
+        temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
 
-	return 0;
+void reverseSentences(char* str) {
+    int length = 0;
+    for (int i = 0; i < str[i] != '\0'; ++i) {
+        length++;
+    }
+    char* start = str;
+    char* end = str;
+
+    for (int i = 0; i <= length; ++i) {
+        if (str[i] == '.' || str[i] == '\0') {
+            end = &str[i - 1];
+
+            reverseString(start, end); 
+
+            start = &str[i + 1];
+        }
+    }
+
+    cout << "Reversed text:" << endl;
+    cout << str << endl;
+}
+
+int main() {
+    const int size = 1000;
+    char str[size];
+
+    cout << "Enter text:" << endl;
+    cin.getline(str, size);
+
+    reverseSentences(str);
+
+    return 0;
 }
