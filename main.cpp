e0dc8feca6087dbb7e8162f095df31aee2db1091
Max <maxstill2006@gmail.com>
2024-06-29 16:41:40 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void countSigns(const char* text, int& countDots, int& countComas) {
+    countDots = 0;
+    countComas = 0;
 
-	return 0;
+    while (*text) {
+        if (*text == '.') {
+            countDots++;
+        }
+        else if (*text == ',') {
+            countComas++;
+        }
+        ++text;
+    }
+}
+
+int main() {
+    const int size = 1000;
+    char text[size];
+
+    cout << "Enter text: ";
+    cin.getline(text, size);
+
+    int countDots, countComas;
+    countSigns(text, countDots, countComas);
+
+    cout << "Dots: " << countDots << endl;
+    cout << "Comas: " << countComas << endl;
+
+    return 0;
 }
