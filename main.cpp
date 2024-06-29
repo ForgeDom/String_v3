16ad478027d8a38fa803fc890ae774a459f0e7ab
Max <maxstill2006@gmail.com>
2024-06-29 16:52:32 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void reverse(char* str) {
+	int length = 0;
+	for (int i = 0; i < str[i] != '\0'; ++i) {
+		length++;
+	}
 
+	for (int i = length; i >=0; --i) {
+		cout << str[i];
+	}
+}
+int main() {
+	const int size = 1000;
+	char str[size];
+
+	cout << "Enter text:" << endl;
+	cin.getline(str, size);
+	reverse(str);
 	return 0;
 }
